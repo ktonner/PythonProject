312600ca63d8462b5d8a07b1470fb241b75dab48
unknown <kathleentonner@gmail.com>
2020-07-02 14:53:55 -0400

    testing

--- dice.py
+++ dice.py
@@ -1,6 +1,11 @@
 import random
 
+
 def roll(n,sides):
-    return tuple((sides) for _ in range(n))
+    print(n)
+    print(sides)
+    for x in range(1,n):
+        print(x)
+        # print(random.randint(1,sides))
 
 roll(1,8)
